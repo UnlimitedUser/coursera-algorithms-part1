512ae9e4725338900f8337f46f65fbff77c5417b
r00t <r00t@e1p66.chisinau.42.fr>
2017-08-14 12:43:36 +0300

    updated week1

--- week1_percolation/Percolation.java
+++ week1_percolation/Percolation.java
@@ -6,6 +6,7 @@ public class Percolation {
     private int[][] map;
     private int top;
     private int bottom;
+    private int open;
 
     public Percolation(int n)                // create n-by-n grid, with all sites blocked
     {
@@ -14,12 +15,13 @@ public class Percolation {
         uf = new WeightedQuickUnionUF((n + 2) * (n + 2));
         top = (n + 2) * (n + 2) - 1;
         bottom = 0;
+        open = 0;
     }
 
     private void   inRange(int i, int j)
     {
         if (i < 1 || i > map.length || j < 1 || j > map.length)
-            throw new IndexOutOfBoundsException("n not in range");
+            throw new java.lang.IllegalArgumentException("n not in range");
     } 
 
     public    void open(int row, int col)    // open site (row, col) if it is not open already
@@ -27,10 +29,13 @@ public class Percolation {
         inRange(row, col);
         if (map[row][col] == 1) return ;
         map[row][col] = 1;
+        open++;
         if (row + 1 < map.length && map[row + 1][col] == 1) uf.union(row * map.length + col, (row + 1) * map.length + col);
         if (col + 1 < map.length && map[row][col + 1] == 1) uf.union(row * map.length + col, row * map.length + col + 1);
         if (row - 1 > 0 && map[row - 1][col] == 1)          uf.union(row * map.length + col, (row - 1) * map.length + col);
         if (row - 1 > 0 && map[row][col - 1] == 1)          uf.union(row * map.length + col, row * map.length + col - 1);
+        if (row == 1)                                       uf.union(map.length + col, top);
+        else if (row == map.length - 1)                     uf.union(row * map.length + col, bottom);
     }
 
     public boolean isOpen(int row, int col)  // is site (row, col) open?
@@ -42,24 +47,14 @@ public class Percolation {
     public boolean isFull(int row, int col)  // is site (row, col) full?
     {
         inRange(row, col);
-        return map[row][col] == 0;
+        return map[row][col] == 1 && uf.connected(top, row * map.length + col);
     }
 
     public     int numberOfOpenSites()       // number of open sites
-    {
-        int s = 0;
-        for (int a = 1; a < map.length; a++)
-            for (int i = 1; i < map.length; i++)
-                if (map[a][i] == 0) s++;
-        return s;
-    }
+    { return open; }
 
     public boolean percolates()              // does the system percolate?
-    {
-        for (int a = 1; a < map.length; a++) if (map[1][a] == 1) uf.union(map.length + a, top);
-        for (int a = 1; a < map.length; a++) if (map[map.length - 1][a] == 1) uf.union(map.length * (map.length - 1) + a, bottom);
-        return uf.connected(top, bottom);
-    }
+    { return uf.connected(top, bottom); }
 
     public static void main(String[] args)   // test client (optional)
     { }
